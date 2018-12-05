4c61783cfcbe2402a741578d002a4f708262ac58
darusca <dario@webnanogy.co.za>
2018-12-05 12:34:37 +0200

    + cannot find vendor/autoload.php

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-require __DIR__.'/../../vendor/autoload.php';
-require __DIR__.'/../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+require '/vendor/autoload.php';
+require '/vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
